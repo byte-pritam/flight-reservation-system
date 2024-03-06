6ef1a1ef9f2872e2a4488b35936bcf98a2dc4514
Pritam Kumar <pritamkumar@Pritams-MBP.lan>
2024-03-06 15:59:42 +0530

    java project structure

---
+++ middleware-java/src/test/java/com/sample/frs/FlightReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.frs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
